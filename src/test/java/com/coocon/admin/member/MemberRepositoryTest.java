4c5c5a0a984c53e661c584df0fd9ee529b54bd8a
YooHoKyun <ghrbs0437@naver.com>
2023-01-27 13:28:24 +0900

    test: MemberRepository 테스트 작성
    1.MemberRepository 테스트 작성
    2.환경별 Datasource 설정

---
+++ src/test/java/com/coocon/admin/member/MemberRepositoryTest.java
@@ -0,0 +1,100 @@
+package com.coocon.admin.member;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.coocon.admin.auth.oauth.Provider;
+import com.coocon.admin.auth.oauth.Role;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.transaction.Transactional;
+
+@DataJpaTest
+//@ActiveProfiles("dev")
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+public class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+    private Member member1;
+
+    @BeforeEach
+    public void beforeEach(){
+        member1 = Member.builder()
+                .userId("defaultId")
+                .name("default")
+                .email("default@coocon.net")
+                .password("1243")
+                .profileImage("")
+                .role(Role.ADMIN)
+                .provider(Provider.COOCON)
+                .accountNonLocked(true)
+                .credentialsNonExpired(true)
+                .build();
+        memberRepository.save(member1);
+    }
+
+    @AfterEach
+    public void afterEach(){
+
+    }
+
+    @Test
+    void insertMember(){
+        //Given
+        Member member = Member.builder()
+                .userId("testId1")
+                .name("test1")
+                .email("test1@coocon.net")
+                .password("testtset")
+                .profileImage("proimage")
+                .role(Role.ADMIN)
+                .provider(Provider.COOCON)
+                .accountNonLocked(true)
+                .credentialsNonExpired(true)
+                .build();
+        //When
+        memberRepository.save(member);
+        //Then
+        Member resultMember=memberRepository.findByUserId("testId1").get();
+        System.out.println(resultMember);
+        System.out.println(member1);
+        System.out.println(memberRepository.findAll());
+        assertThat(resultMember).isEqualTo(member);
+
+    }
+
+    @Test
+    void updateMember(){
+        //given
+        Member existMember=memberRepository.findByUserId("defaultId").get();
+        //when
+        existMember.setName("updated");
+        existMember.setEmail("updated");
+        memberRepository.save(existMember);
+        //then
+        System.out.println(memberRepository.findAll());
+        assertThat(existMember);
+    }
+
+    @Test
+    void deleteMember(){
+        //given
+        Member existMember=memberRepository.findByUserId("defaultId").get();
+        //when
+        memberRepository.delete(existMember);
+        //then
+        System.out.println("############");
+        System.out.println(memberRepository.findAll());
+    }
+
+
+
+}
